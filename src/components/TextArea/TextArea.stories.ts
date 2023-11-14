08cfd4f6abd109aec40f016b83d1ce93af6ae43d
Massolinii <massimilianoesposito548@gmail.com>
2023-11-14 23:12:41 +0100

    this works lmao

--- src/components/TextArea/TextArea.stories.ts
+++ src/components/TextArea/TextArea.stories.ts
@@ -1,21 +1,53 @@
-// TextArea.stories.ts
-import React from 'react';
-import { Meta, StoryObj } from '@storybook/react';
 import { TextArea } from '../TextArea/TextArea.tsx';
 
 export default { component: TextArea };
 
-export const Primary = { args: { } };
+export const Basic = { args: { } };
 
-export const WithCustomStyles = {
-  ...Primary,
+export const WithLabel = {
   args: {
-    ...Primary.args,
-    padding: '20px',
-    margin: '15px',
-    textAlign: 'center',
-    letterSpacing: '1px',
-    lineHeight: '1.5',
-    backgroundColor: 'lightgrey',
+    label: 'Your Message',
+    labelWeight: 'bold',
+    labelColor: 'blue',
+    placeholder: 'Type here...',
+  },
+};
+
+export const StylishTextArea = {
+  args: {
+    padding: '15px',
+    backgroundColor: 'lightyellow',
+    borderColor: 'green',
+    borderRadius: '10px',
+    borderWidth: '2px',
+    borderStyle: 'solid',
+    textColor: 'darkgreen',
+    placeholder: 'Enter text...',
+    label: 'Stylish TextArea',
+    labelColor: 'darkgreen',
+  },
+};
+
+export const DarkModeTextArea = {
+  args: {
+    backgroundColor: '#333',
+    textColor: 'white',
+    borderColor: 'grey',
+    borderRadius: '5px',
+    label: 'Dark Mode',
+    labelColor: 'white',
+    focusOutline: '2px solid cyan',
+    placeholder: 'Dark mode...',
+  },
+};
+
+export const CustomScrollbar = {
+  args: {
+    backgroundColor: '#f2f2f2',
+    scrollbarColor: 'darkgrey blue',
+    scrollbarWidth: 'thin',
+    label: 'Scrollable TextArea',
+    labelColor: 'black',
+    placeholder: 'Scroll me...',
   },
 };
--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,6 +1,4 @@
-// TextArea.tsx
 import React from "react";
-import "./TextArea.css"; // Only if needed for styles not covered by props
 
 interface TextAreaProps {
   padding?: string;
@@ -9,6 +7,18 @@ interface TextAreaProps {
   letterSpacing?: string;
   lineHeight?: string;
   backgroundColor?: string;
+  rows?: number;
+  label?: string;
+  labelWeight?: string;
+  labelColor?: string;
+  textColor?: string;
+  borderRadius?: string;
+  borderWidth?: string;
+  borderColor?: string;
+  borderStyle?: string;
+  placeholder?: string;
+  scrollbarColor?: string;
+  focusOutline?: string;
 }
 
 const TextArea: React.FC<TextAreaProps> = ({
@@ -18,18 +28,52 @@ const TextArea: React.FC<TextAreaProps> = ({
   letterSpacing,
   lineHeight,
   backgroundColor,
+  rows = 3,
+  label,
+  labelWeight,
+  labelColor,
+  textColor,
+  borderRadius,
+  borderWidth,
+  borderColor,
+  borderStyle,
+  placeholder,
+  scrollbarColor,
+  focusOutline,
   ...props
 }) => {
-  const style = {
+  const textAreaStyle = {
     padding,
     margin,
     textAlign,
     letterSpacing,
     lineHeight,
     backgroundColor,
+    color: textColor,
+    borderRadius,
+    borderWidth,
+    borderColor,
+    borderStyle,
+    outline: focusOutline,
+    overflow: "auto",
+    scrollbarColor,
   };
 
-  return <textarea style={style} {...props}></textarea>;
+  return (
+    <div>
+      {label && (
+        <label style={{ fontWeight: labelWeight, color: labelColor }}>
+          {label}
+        </label>
+      )}
+      <textarea
+        rows={rows}
+        style={textAreaStyle}
+        placeholder={placeholder}
+        {...props}
+      ></textarea>
+    </div>
+  );
 };
 
 export { TextArea };
